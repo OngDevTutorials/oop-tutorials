1bf5aefd14fc1c6936ff696c814e3907f0334f02
Duy Nguyen <milonguyen95@outlook.com>
2019-04-10 21:44:35 +0700

    basic pizza store

---
+++ PizzaStore/PizzaStore/Pizza.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaStore
+{
+    public abstract class Pizza
+    {
+        protected string name;
+        protected string dough;
+        protected string sauce;
+        protected List<string> toppings = new List<string>();
+
+        public void Prepare()
+        {
+            Console.WriteLine("Preparing " + name);
+            Console.WriteLine("Tossing dough..." + dough);
+            Console.WriteLine("Adding sauce..." + sauce);
+            Console.WriteLine("Adding toppings:");
+            toppings?.ForEach(
+                (topping) => Console.WriteLine("\t" + topping)
+            );
+        }
+
+        public void Bake()
+        {
+            Console.WriteLine("Baking " + name + "in 30 mins");
+        }
+
+        public void Cut()
+        {
+            Console.WriteLine("Cutting " + name);
+        }
+
+        public virtual void Box()
+        {
+            Console.WriteLine("Boxing " + name);
+        }
+    }
+}
---
+++ PizzaStore/PizzaStore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pizzaStore = new PizzaStore();
+            pizzaStore.OrderPizza("cheese");
+            pizzaStore.OrderPizza("greek");
+            pizzaStore.OrderPizza("pepperoni");
+            pizzaStore.OrderPizza("ongdev");
+        }
+    }
+}
